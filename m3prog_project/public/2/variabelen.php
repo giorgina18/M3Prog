d2cd6480e78f452916ab1f2b9f6146900ca84fdb
giorgina18 <144676972+giorgina18@users.noreply.github.com>
2024-03-05 15:15:03 +0100

    variabelen2

---
+++ m3prog_project/public/2/variabelen.php
@@ -0,0 +1,21 @@
+<?php
+
+
+//3variabelen//
+    $temperatuur = 25.6;
+    $stad = "Amsterdam";
+    $regen = "3mm";
+
+
+    $auto2	= "bmw M5";
+    $_naam	= "giorgina";
+    //$auto's =	; ( die 's telt niet mee )
+    //$auto/onderdelen = ;( nee het werkt niet door de /)
+    //$huis&tuin = ; ( die huis werkt wel maar na &tuin werkt niet)
+    $naw = "rawr";
+    //$22 =	; ( 22 is getal dat kan niet)
+    //$$auto	=; ( 2 $$ werkt niet)
+    $kleurVanAuto= "roze";	
+    //$software-dev = ; (  na dat streepje -dev kan niet )
+
+?>
