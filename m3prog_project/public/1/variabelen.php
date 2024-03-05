678ebe3008900450b1ec5584481b09c8f478cb0d
giorgina18 <144676972+giorgina18@users.noreply.github.com>
2024-03-05 14:25:10 +0100

    variabelen

---
+++ m3prog_project/public/1/variabelen.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>variabelen</title>
+</head>
+<body>
+    <?php
+    
+    $naam = "giorgina";
+    $woonplaats = "Alkmaar";
+    $leeftijd = 18;
+    $temparatuur = 9.5;
+
+    echo"Ik ben $naam, Ik ben $leeftijd jaar oud en werk in Horeca. ik woon in $woonplaats. In de Horeca kan het best wel koud zijn in vriezer zoals $temparatuur ";
+    
+    
+    
+   
+   
+    ?>
+</body>
+</html>
