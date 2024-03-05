2edab67f47365a30569838bd52b5365fe06814f0
giorgina18 <144676972+giorgina18@users.noreply.github.com>
2024-03-05 15:17:45 +0100

    variabelen_beter

--- m3prog_project/public/2/variabelen.php
+++ m3prog_project/public/2/variabelen.php
@@ -7,15 +7,19 @@
     $regen = "3mm";
 
 
-    $auto2	= "bmw M5";
-    $_naam	= "giorgina";
+    $auto2	= "<br>bmw M5";
+    $_naam	= "<br>giorgina";
     //$auto's =	; ( die 's telt niet mee )
     //$auto/onderdelen = ;( nee het werkt niet door de /)
     //$huis&tuin = ; ( die huis werkt wel maar na &tuin werkt niet)
-    $naw = "rawr";
+    $naw = "<br>rawr";
     //$22 =	; ( 22 is getal dat kan niet)
     //$$auto	=; ( 2 $$ werkt niet)
-    $kleurVanAuto= "roze";	
+    $kleurVanAuto= "<br>roze";	
     //$software-dev = ; (  na dat streepje -dev kan niet )
 
+    echo"$auto2";
+    echo"$_naam";
+    echo"$naw";
+    echo"$kleurVanAuto"
 ?>
