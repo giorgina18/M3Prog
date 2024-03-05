5da8de475fdd6cd6397f1f1ac8a23c81ba0a42fe
giorgina18 <144676972+giorgina18@users.noreply.github.com>
2024-03-05 14:52:55 +0100

    datatypes_2

---
+++ m3prog_project/public/2/datatypes.php
@@ -0,0 +1,17 @@
+<?php 
+
+$huisnummer = 30;
+echo"<br>mijn huisnummer is $huisnummer";
+
+$rijbewijs = "geslaagd";
+echo"<br>jij bent $rijbewijs!!";
+
+$prijs = 2.13;
+echo"<br>een liter benzine kost $prijs";
+
+$boolean = "true" ;
+echo"<br> mijn zusje is 18. $boolean";
+
+
+
+?>
