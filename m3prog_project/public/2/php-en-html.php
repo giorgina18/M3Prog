c5140918e2be648b36560dd09f47156944b27c9b
giorgina18 <144676972+giorgina18@users.noreply.github.com>
2024-03-12 14:42:37 +0100

    php_html_beter

--- m3prog_project/public/2/php-en-html.php
+++ m3prog_project/public/2/php-en-html.php
@@ -1,30 +1,23 @@
 
-    <?php
-    $titel = "php-html";
-    $url = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fyody.vn%2Fgigachad-meme-la-gi&psig=AOvVaw1I5A_kVJyYq56bsXUBmpyH&ust=1710336552671000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCLDZ-afq7oQDFQAAAAAdAAAAABAJ";
-    $foto_titel = "gigaaaCHAAADDDDD";
-    $auteur = "giorgina18";
-    ?>
-
-
+<?php
+    $titelpagina = "big strong kitty";
+    $image = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c7a6c7ac-f063-4285-808f-c02182e1b572/dgp8kzr-bdd97f73-63ae-4a11-accb-023012062757.jpg/v1/fit/w_828,h_494,q_70,strp/no_gigachad___by_modernaustriaartis_dgp8kzr-414w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzM2IiwicGF0aCI6IlwvZlwvYzdhNmM3YWMtZjA2My00Mjg1LTgwOGYtYzAyMTgyZTFiNTcyXC9kZ3A4a3pyLWJkZDk3ZjczLTYzYWUtNGExMS1hY2NiLTAyMzAxMjA2Mjc1Ny5qcGciLCJ3aWR0aCI6Ijw9MTIzMiJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.Ul1hsF28USEBsk7P-j7NB4jPR91yVHcGWiQShAPLb0M";
+    $Titel = "strong kitty";
+    $auteur = "giorgina18"
+?>
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>php-html</title>
+    <title>kitty CHAAAAAD</title>
 </head>
-
 <body>
-
-    <main> 
-    <h1> <?php echo $titel ?></h1>
-    <img src=""<?php echo $url ?> alt="<?php echo $foto_titel?>" />
+    <main>
+    <h1><?php echo $titelpagina; ?></h1>
+    <img src="<?php echo $image?>" alt="<?php echo $Titel?>" />
     <h2><?php echo $auteur; ?></h2>
-</main>
-
+    </main>
 </body>
-
 </html>
