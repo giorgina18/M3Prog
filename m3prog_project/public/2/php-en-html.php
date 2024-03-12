934150ba83dc8a7950a85112c195d96d2e8d1828
giorgina18 <144676972+giorgina18@users.noreply.github.com>
2024-03-12 14:31:48 +0100

    html_php

---
+++ m3prog_project/public/2/php-en-html.php
@@ -0,0 +1,30 @@
+
+    <?php
+    $titel = "php-html";
+    $url = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fyody.vn%2Fgigachad-meme-la-gi&psig=AOvVaw1I5A_kVJyYq56bsXUBmpyH&ust=1710336552671000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCLDZ-afq7oQDFQAAAAAdAAAAABAJ";
+    $foto_titel = "gigaaaCHAAADDDDD";
+    $auteur = "giorgina18";
+    ?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-html</title>
+</head>
+
+<body>
+
+    <main> 
+    <h1> <?php echo $titel ?></h1>
+    <img src=""<?php echo $url ?> alt="<?php echo $foto_titel?>" />
+    <h2><?php echo $auteur; ?></h2>
+</main>
+
+</body>
+
+</html>
--- m3prog_project/public/2/variabelen.php
+++ m3prog_project/public/2/variabelen.php
@@ -2,24 +2,28 @@
 
 
 //3variabelen//
-    $temperatuur = 25.6;
-    $stad = "Amsterdam";
-    $regen = "3mm";
+$temperatuur = 25.6;
+$stad = "Amsterdam";
+$regen = "3mm";
 
 
-    $auto2	= "<br>bmw M5";
-    $_naam	= "<br>giorgina";
-    //$auto's =	; ( die 's telt niet mee )
-    //$auto/onderdelen = ;( nee het werkt niet door de /)
-    //$huis&tuin = ; ( die huis werkt wel maar na &tuin werkt niet)
-    $naw = "<br>rawr";
-    //$22 =	; ( 22 is getal dat kan niet)
-    //$$auto	=; ( 2 $$ werkt niet)
-    $kleurVanAuto= "<br>roze";	
-    //$software-dev = ; (  na dat streepje -dev kan niet )
+$auto2    = "<br>bmw M5";
+$_naam    = "<br>giorgina";
+//$auto's =	; ( die 's telt niet mee )
+//$auto/onderdelen = ;( nee het werkt niet door de /)
+//$huis&tuin = ; ( die huis werkt wel maar na &tuin werkt niet)
+$naw = "<br>rawr";
+//$22 =	; ( 22 is getal dat kan niet)
+//$$auto	=; ( 2 $$ werkt niet)
+$kleurVanAuto = "<br>roze";
+//$software-dev = ; (  na dat streepje -dev kan niet )
 
-    echo"$auto2";
-    echo"$_naam";
-    echo"$naw";
-    echo"$kleurVanAuto"
-?>
+
+
+echo "$auto2";
+echo "$_naam";
+echo "$naw";
+echo "$kleurVanAuto"
+
+
+ ?>
