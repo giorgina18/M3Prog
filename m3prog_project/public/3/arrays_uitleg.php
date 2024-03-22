34a6ffaedca2dc34b3f833b653b8e12f6c04ae7c
giorgina18 <144676972+giorgina18@users.noreply.github.com>
2024-03-22 10:44:14 +0100

    arrays

---
+++ m3prog_project/public/3/arrays_uitleg.php
@@ -0,0 +1,16 @@
+<?php
+$zangers = ["<br> lady gaga", "<br>prince", "<br>drake", "<br>angerfist", "<br>vieze asbak", "<br>vieze baap"];
+
+print_r($zangers);
+
+echo $zangers[3];
+echo  $zangers[5];
+
+count($zangers);
+
+array_push($zangers, "ariana grande");
+
+echo implode($zangers);
+print_r($zangers);
+
+?>
